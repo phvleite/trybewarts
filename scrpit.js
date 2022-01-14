2704c1342ac1e5e563d4b6cc079fd432ff8a4714
phvleite <phvleite@gmail.com>
2022-01-14 12:14:55 -0300

    requisitos aprovados até o #19

--- scrpit.js
+++ scrpit.js
@@ -3,6 +3,10 @@
 const inputEmail = document.getElementById('email');
 const inputPassword = document.getElementById('password');
 const btEntrar = document.getElementById('entrar');
+const ckbAgreement = document.getElementById('agreement');
+const submitBtn = document.getElementById('submit-btn');
+
+submitBtn.disabled = true;
 
 function entrarEscola() {
   if (inputEmail.value === '' || inputPassword.value === '') {
@@ -13,6 +17,25 @@ function entrarEscola() {
   }
 }
 
+function ativaEnviar() {
+  submitBtn.disabled = false;
+  submitBtn.setAttribute('class', 'submit-btn');
+}
+
+function desativarEnviar() {
+  submitBtn.disabled = true;
+  submitBtn.setAttribute('class', 'submit-disable');
+}
+
+function verificaCkbAgreement() {
+  if (ckbAgreement.checked) {
+    ativaEnviar();
+  } else {
+    desativarEnviar();
+  }
+}
+
 btEntrar.addEventListener('click', entrarEscola);
+ckbAgreement.addEventListener('click', verificaCkbAgreement);
 
 // Fim verificação de email e senha
